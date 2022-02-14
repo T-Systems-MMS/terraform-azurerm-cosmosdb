798d6dbd46e0a2d6622e1a71252314dfa93fcc20
mima <Michaela.Mattes@t-systems.com>
2022-02-14 16:08:10 +0100

    add output

---
+++ outputs.tf
@@ -0,0 +1,10 @@
+output "cosmosdb_account" {
+  description = "azurerm_cosmosdb_account results"
+  value = {
+    for cosmosdb_account in keys(azurerm_cosmosdb_account.cosmosdb_account) :
+    cosmosdb_account => {
+      id = azurerm_cosmosdb_account.cosmosdb_account[cosmosdb_account].id
+      name = azurerm_cosmosdb_account.cosmosdb_account[cosmosdb_account].name
+    }
+  }
+}
