2338ac95e7efef6a4d9499d424f25a4e6c43c098
schurzi <Martin.Schurz@telekom.de>
2024-08-28 13:10:18 +0200

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "registry.terraform.io/hashicorp/azurerm"
-      version = ">=3.11.0"
+      version = ">=3.11.0, <4.0"
     }
   }
   required_version = ">=1.0"
